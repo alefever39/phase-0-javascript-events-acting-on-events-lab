b0e82e3cbd649fee01509ee3c88aa21c851d6182
Allie LeFever <alefever39@gmail.com>
2022-05-20 10:01:43 -0600

    commit tree 6d999c4b08a41aa33979154c7202063bb751d277

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const updateLeft = parseInt(leftNumbers, 10);
+    if (updateLeft > 0) {
+        dodger.style.left = `${updateLeft - 5}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const updateLeft = parseInt(leftNumbers, 10);
+    if (updateLeft < 360) {
+        dodger.style.left = `${updateLeft + 5}px`;
+    }
+}
+
+document.addEventListener('keydown', e => {
+    if(e.key === "ArrowLeft") {
+        moveDodgerLeft()
+    }
+    if(e.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+})
